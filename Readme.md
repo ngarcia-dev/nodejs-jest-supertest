00f549c70a1f637b5a87699a6b46be0c8297e1ce
ngarcia <garcianahuel77@gmail.com>
2023-12-05 14:59:40 -0300

    testing endpoint post tasks

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 # Testing with JestJS and SuperTest
+
+#### _npm test_ para ejecutar en windows
+
+#### _nmp run dev_ para ejecutar en linux
--- test/index.spec.js
+++ test/index.spec.js
@@ -12,3 +12,43 @@ describe("GET /tasks", () => {
     expect(response.body).toBeInstanceOf(Array);
   });
 });
+
+describe("POST /tasks", () => {
+  describe("given a title and description", () => {
+    const newTask = {
+      title: "test task",
+      description: "test description",
+    };
+    // should respond with a 200 status code
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/tasks").send(newTask);
+      expect(response.statusCode).toBe(200);
+    });
+    // should respond with a content-type of application/json
+    test("should have a content-type: application/json in header", async () => {
+      const response = await request(app).post("/tasks").send(newTask);
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json")
+      );
+    });
+    // should respond with a json object containing the new task with an id
+    test("should respond with an task ID", async () => {
+      const response = await request(app).post("/tasks").send(newTask);
+      expect(response.body.id).toBeDefined();
+    });
+  });
+  describe("when title and description is missing", () => {
+    test("should respond with a 400 status code", async () => {
+      const fields = [
+        {},
+        { title: "test task" },
+        { description: "test description" },
+      ];
+
+      for (const body of fields) {
+        const response = await request(app).post("/tasks").send(body);
+        expect(response.statusCode).toBe(400);
+      }
+    });
+  });
+});
